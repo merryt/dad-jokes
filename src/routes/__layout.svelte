85b331eaa0867bce84e3792a04622ebf47b61c95
merryt <tymerry@gmail.com>
2022-07-04 19:48:50 -0600

    rough draft

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../app.css";
+  import Background from "$lib/Background.svelte";
+  import Index from "./index.svelte";
+</script>
+
+<Background />
+<div class="containerWrapper">
+  <main class="container">
+    <slot />
+  </main>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount } from "svelte";
+  let data;
+
+  const getNewJoke = async () => {
+    let response = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    data = response.ok ? await response.json() : {};
+  };
+  onMount(() => {
+    getNewJoke();
+  });
+</script>
+
+{#await data}
+  loading
+{:then value}
+  {#if value}
+    <h1>{value.joke}</h1>
+  {/if}
+{/await}
+
+<button on:click="{getNewJoke}">New Joke</button>
+<p>
+  All jokes are from <a href="https://icanhazdadjoke.com/">icanhazdadjoke.com</a
+  >.
+</p>
+<p>
+  Background by <a href="https://codepen.io/chriscoyier/pen/nZWbMN"
+    >Chris Coyier</a
+  >.
+</p>
